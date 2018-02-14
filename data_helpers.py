9f03b354af27c5d666068de868f0fc3a36162ef1
Anshuman Sahoo <anshuman264@gmail.com>
2018-02-13 23:28:54 -0500

    update helper

--- data_helpers.py
+++ data_helpers.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 import re
 import itertools
 from collections import Counter
@@ -30,18 +31,15 @@ def load_data_and_labels(positive_data_file, negative_data_file):
     Loads MR polarity data from files, splits the data into words and generates labels.
     Returns split sentences and labels.
     """
+    df = pd.read_csv("/Users/anshumansahoo/Documents/GitHub/kaggle/toxic_comments/train.csv", index_col=None, header=0)
+
     # Load data from files
-    positive_examples = list(open(positive_data_file, "r").readlines())
-    positive_examples = [s.strip() for s in positive_examples]
-    negative_examples = list(open(negative_data_file, "r").readlines())
-    negative_examples = [s.strip() for s in negative_examples]
     # Split by words
-    x_text = positive_examples + negative_examples
+    x_text = df["comment_text"].tolist()
+    x_text = [s.strip() for s in x_text]
     x_text = [clean_str(sent) for sent in x_text]
     # Generate labels
-    positive_labels = [[0, 1] for _ in positive_examples]
-    negative_labels = [[1, 0] for _ in negative_examples]
-    y = np.concatenate([positive_labels, negative_labels], 0)
+    y = df[["toxic","severe_toxic","obscene","threat","insult","identity_hate"]].as_matrix()
     return [x_text, y]
 
 
